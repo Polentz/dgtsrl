482ee7328f345792a8a6a7e259513407c1ad7821
Giulia Polenta <giuliapolenta@gmail.com>
2026-05-29 08:33:57 +0200

    Commit

--- site/config/config.php
+++ site/config/config.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'debug'  => true,
+    'debug' => true,
     'jr' => [
         'static_site_generator' => [
-            'endpoint'   => 'static', // enables the trigger URL & Panel button
+            'endpoint' => 'static', // enables the trigger URL & Panel button
             'output_folder' => './docs',           // where the static files will be generated
-            'base_url'   => './',                     // change if site isn't at domain root
-            'preserve'   => ['README.md', '.git'],   // files/folders to keep on regeneration
+            'base_url' => './',                     // change if site isn't at domain root
+            'preserve' => ['README.md', '.git'],   // files/folders to keep on regeneration
             'skip_media' => false,                   // set true if media is already on a CDN
         ]
     ]
