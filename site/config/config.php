84e2b826e6798575cc9bdbecdbf2374ae9ade7c9
Giulia Polenta <giuliapolenta@gmail.com>
2026-05-29 12:37:18 +0200

    Final config file for static site generator

--- site/config/config.php
+++ site/config/config.php
@@ -6,7 +6,7 @@ return [
         'static_site_generator' => [
             'endpoint' => 'static', // enables the trigger URL & Panel button
             'output_folder' => './docs',           // where the static files will be generated
-            'base_url' => './',                     // change if site isn't at domain root
+            'base_url' => 'file',                   // relative links + /index.html, work when opened directly from disk or hosted at any path
             'preserve' => ['README.md', '.git'],   // files/folders to keep on regeneration
             'skip_media' => false,                   // set true if media is already on a CDN
         ]
